aef57d47a6a39f9fc51b6ec511e0e4c4a11f5414
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-07-24 22:29:25 +0700

    add more cases to TestPage.tsx

--- example/TestPage.tsx
+++ example/TestPage.tsx
@@ -1,11 +1,21 @@
 import { useQueryParams } from '../src';
-import { StringParam } from 'serialize-query-params';
+import {
+  BooleanParam,
+  DateParam,
+  DelimitedArrayParam,
+  ObjectParam,
+  StringParam,
+} from 'serialize-query-params';
 import { Link } from 'react-router-dom';
 import { Links } from './Links';
 
 export const TestPage = () => {
   const [queryParams, setQueryParams] = useQueryParams({
     sortBy: StringParam,
+    bool: BooleanParam,
+    date: DateParam,
+    obj: ObjectParam,
+    delimitedArray: DelimitedArrayParam,
   });
   return (
     <div>
@@ -25,6 +35,74 @@ export const TestPage = () => {
           }}
         >
           Set to 'asd'
+        </button>
+      </div>
+      <div>
+        boolean: {queryParams.bool === true ? 'true' : 'false'}
+        <button
+          onClick={() => {
+            setQueryParams({ bool: true });
+          }}
+        >
+          Set to true
+        </button>
+        <button
+          onClick={() => {
+            setQueryParams({ bool: false });
+          }}
+        >
+          Set to false
+        </button>
+      </div>
+      <div>
+        date: {queryParams.date?.toString()}
+        <button
+          onClick={() => {
+            setQueryParams({ date: new Date(2020, 10, 2) });
+          }}
+        >
+          Set to 2020-10-02
+        </button>
+        <button
+          onClick={() => {
+            setQueryParams({ date: new Date(2021, 11, 1) });
+          }}
+        >
+          Set to 2021-11-01
+        </button>
+      </div>
+      <div>
+        obj: {JSON.stringify(queryParams.obj)}
+        <button
+          onClick={() => {
+            setQueryParams({ obj: { asd: 'poi', tyu: 'g1', rty: 'uu' } });
+          }}
+        >
+          Set to {JSON.stringify({ asd: 'poi', tyu: 'g1', rty: 'uu' })}
+        </button>
+        <button
+          onClick={() => {
+            setQueryParams({ obj: { asd: 'zxc', tyu: '1' } });
+          }}
+        >
+          Set to {JSON.stringify({ asd: 'zxc', tyu: '1' })}
+        </button>
+      </div>
+      <div>
+        delimitedArray: {JSON.stringify(queryParams.delimitedArray)}
+        <button
+          onClick={() => {
+            setQueryParams({ delimitedArray: ['asd', 'zxc'] });
+          }}
+        >
+          Set to ['asd','zxc']
+        </button>
+        <button
+          onClick={() => {
+            setQueryParams({ delimitedArray: ['qwe', '123'] });
+          }}
+        >
+          Set to ['qwe', '123']
         </button>
       </div>
     </div>
